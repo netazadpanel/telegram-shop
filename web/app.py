6092f2315096fa1857fcaa494b8d121a24ac2517
netazadpanel <javadti.a@gmail.com>
2026-02-01 00:43:12 +0330

    Update app.py

--- web/app.py
+++ web/app.py
@@ -6,7 +6,7 @@ from config import ADMIN_PASSWORD
 app = Flask(__name__)
 app.secret_key = "super_secret_key"
 
-DB_PATH = "database/store.db"
+DB_PATH = "store.db"
 
 
 # ---------------------- دیتابیس ----------------------
@@ -245,4 +245,5 @@ def confirm(order_id):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
