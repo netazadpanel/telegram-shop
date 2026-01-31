e76bd8a87292c65f6207d059b8ef0c115db6164a
netazadpanel <javadti.a@gmail.com>
2026-02-01 00:17:45 +0330

    Update app.py

--- web/app.py
+++ web/app.py
@@ -244,5 +244,4 @@ def confirm(order_id):
 # ---------------------- اجرا ----------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
